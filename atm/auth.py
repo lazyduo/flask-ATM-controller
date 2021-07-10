8fd6be58ffe763119215509bb49628af527a3025
lazyduo <dadanuna@gmail.com>
2021-07-10 23:55:40 +0900

    fork from flask-tutorial

---
+++ atm/auth.py
@@ -0,0 +1,93 @@
+import functools
+
+from flask import(
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from atm.db import get_db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        card_number = request.form['card_number']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not card_number:
+            error = 'Card_number is required.'
+        elif not username:
+            error = 'Usernname is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM account WHERE card_number = ?', (card_number,)
+        ).fetchone() is not None:
+            error = f"Card Number {card_number} is already registered."
+        
+        if error is None:
+            db.execute(
+                'INSERT INTO account (username, card_number, password) VALUES (?, ?, ?)',
+                (username, card_number, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+        
+        flash(error)
+    
+    return render_template('auth/register.html')
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        card_number = request.form['card_number']
+        pin_number = request.form['pin_number']
+        db = get_db()
+        error = None
+        account = db.execute(
+            'SELECT * FROM account WHERE card_number = ?', (card_number,)
+        ).fetchone()
+
+        if account is None:
+            error = 'Incorrect Card number.'
+        elif not check_password_hash(account['pin_number'], pin_number):
+            error = 'Incorrect PIN number.'
+        if error is None:
+            session.clear()
+            session['account_id'] = account['id']
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    account_id = session.get('account_id')
+
+    if account_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM account WHERE id = ?', (account_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        
+        return view(**kwargs)
+    
+    return wrapped_view
---
+++ atm/schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS account;
+
+CREATE TABLE account (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT NOT NULL,
+  card_number TEXT UNIQUE NOT NULL,
+  pin_number TEXT NOT NULL
+);
